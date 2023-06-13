d00a0797dccecb28b38da77483345752db87efc0
Cindy Le <cindee.le@gmail.com>
2023-06-13 07:41:50 +0000

    set up route and auth middleware

--- client/src/Tabs.js
+++ client/src/Tabs.js
@@ -45,21 +45,6 @@ export default function Tabs() {
           <Link to="/map">Map</Link>
         </div>
       </div>
-
-      {/* <ul className="nav nav-tabs">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Active</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
-        </li>
-      </ul> */}
     </div>
   );
 }
